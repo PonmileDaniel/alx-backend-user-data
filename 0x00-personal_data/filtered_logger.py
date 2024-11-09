03255c829f1a2b30558231030b9f387d271c8754
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-09 20:20:43 +0100

    Updated the filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ A module to filter personal data fields in log messages.
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -16,3 +17,27 @@ def filter_datum(
     for field in fields:
         message = re.sub(f"(?<={field}=)[^{separator}]*", redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter with fields to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record, redacting sensitive fields
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
