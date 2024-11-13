89a6e65940f7f891f3d8b9a635463e0ac4f36b40
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-13 04:23:19 +0100

    Error handling for authorization

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """Endpoint to trigger a 401 unauthorixzed
+    error"""
+    abort(401)
