2c6c1b8e330023afc5d1c347ac6e02cc04fb5c86
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-16 22:33:36 +0100

    Implemented the request func

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to check if auth is required"""
-        return False
+        if not path or not excluded_paths:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                return False
+            elif path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method to get authorization header"""
