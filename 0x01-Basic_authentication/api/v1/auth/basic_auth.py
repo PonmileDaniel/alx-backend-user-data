8d2117e39a487966e54825ef5334862948772d4e
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-18 14:55:12 +0100

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
         Extract the Base64 part
         """
