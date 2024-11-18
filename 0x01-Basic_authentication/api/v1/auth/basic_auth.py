40a5cbb75c23836a3efac2e96e64cb66f5e46e21
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-18 15:12:30 +0100

    Basic  - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Returns decoded value of Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_bytes = base64.b64decode(base64_authorization_header)
+            return decode_bytes.decode("utf-8")
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
