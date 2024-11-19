0ecde488145647cc092d57ef7912a958a1d3a25d
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-19 14:54:08 +0100

    Pycodestyle fixing

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth"""
 
     user_id_by_session_id = {}
+
     def __init__(self):
         super().__init__()
 
@@ -19,7 +20,7 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
