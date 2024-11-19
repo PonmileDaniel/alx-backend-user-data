44d9c3d2e6e54fcec3488aa4103568bcaaf4ae2f
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-19 18:24:28 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,11 +57,14 @@ def before_request():
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/"
+
     ]
 
     if auth and auth.require_auth(request.path, authorized_list):
         if not auth.authorization_header(request):
-            abort(401)
+            if not auth.session_cookie(request):
+                abort(401)
         request.current_user = auth.current_user(request)
         if not auth.current_user(request):
             abort(403)
