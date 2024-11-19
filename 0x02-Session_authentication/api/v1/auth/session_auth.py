0717cf1481488115713c90e5ad123802fb3881a8
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-19 14:49:44 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,22 @@
 cDocDoc
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth"""
 
+    user_id_by_session_id = {}
     def __init__(self):
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
