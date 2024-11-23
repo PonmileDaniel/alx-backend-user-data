7c1dbf9995a9bc625f3c22352eb381137f8a081a
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-23 17:37:28 +0100

    Missplet a word

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -20,6 +20,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
-    hased_password = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
