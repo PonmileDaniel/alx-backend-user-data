951ca6f93034f1dd13802ffe2e82262b68b4ef46
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-24 21:07:01 +0100

    Hased password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,10 +8,11 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-        """
-        Hash a password with bcrypt and return the hashed password"""
+    """
+    Hash a password with bcrypt and return the hashed password
+    """
 
-        if not isinstance(password, str):
-            raise TypeError("Password must be a string.")
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        return hashed_password
+    if not isinstance(password, str):
+        raise TypeError("Password must be a string.")
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return hashed_password
